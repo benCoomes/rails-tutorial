337cb949b08bd96faa9bc47d356ec57a178f4635
Ben Coomes <bencoomes@github.com>
2022-06-11 02:16:16 +0000

    Show user's posts on user page

--- toy_app/app/controllers/users_controller.rb
+++ toy_app/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[ edit update destroy ]
 
   # GET /users or /users.json
   def index
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.eager_load(:microposts).find(params[:id])
   end
 
   # GET /users/new
