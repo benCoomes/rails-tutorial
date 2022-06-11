57a07fed06f0df06b196c24d7ae999dfb73202ce
Ben Coomes <bencoomes@github.com>
2022-06-11 18:11:29 +0000

    Set root route

---
+++ sample_app/test/controllers/root_test.rb
@@ -0,0 +1,9 @@
+class RootTest < ActionDispatch::IntegrationTest
+  test "root should be home" do
+    # todo: better way to share this logic with staic pages tests,
+    # without moving this test to that file?
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | MP"
+  end
+end
