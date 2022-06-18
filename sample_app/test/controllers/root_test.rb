5f4ade5d9acea3863cea160e413cc5e38b3feb30
Ben Coomes <bencoomes@github.com>
2022-06-18 20:19:29 +0000

    Hide static_pages from routes and use '_path' variables. Replace home path with root path.

--- sample_app/test/controllers/root_test.rb
+++
@@ -1,9 +0,0 @@
-class RootTest < ActionDispatch::IntegrationTest
-  test "root should be home" do
-    # todo: better way to share this logic with staic pages tests,
-    # without moving this test to that file?
-    get root_url
-    assert_response :success
-    assert_select "title", "MICROPOSTS"
-  end
-end
