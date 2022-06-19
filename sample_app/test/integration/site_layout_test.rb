5f44780abf8a51293270521930f44d769abd81ba
Ben Coomes <bencoomes@github.com>
2022-06-19 21:04:59 +0000

    add tests for signup page

--- sample_app/test/integration/site_layout_test.rb
+++ sample_app/test/integration/site_layout_test.rb
@@ -5,8 +5,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template "static_pages/home"
     assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", helf_path
+    assert_select "a[href=?]", helf_path # count is optional. Any count accepted if not set 
     assert_select "a[href=?]", about_path, count: 2
     assert_select "a[href=?]", contact_path, count: 1
+    assert_select "a[href=?]", signup_path, count: 1
   end
 end
