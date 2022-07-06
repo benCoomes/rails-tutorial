48c6b9d12e63310e5fbaa096ce14deb1d30b6ed2
Ben Coomes <bencoomes@github.com>
2022-07-06 14:12:28 +0000

    add gravatar to user profile

--- sample_app/app/helpers/users_helper.rb
+++ sample_app/app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for(user, size: 80)
+    grav_id = Digest::MD5::hexdigest(user.email.downcase)
+    grav_url = "https://secure.gravatar.com/avatar/#{grav_id}?s=#{size}"
+    image_tag(grav_url, alt: user.name, class: "gravatar")
+  end
 end
